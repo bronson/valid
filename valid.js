66665b88da2b3376cd0a2ed7d0e82ef39378b076
Scott Bronson <brons_git@rinspin.com>
2011-09-17 08:57:15 -0700

    validation commands are now test(), check(), and verify()

--- valid.js
+++ valid.js
@@ -23,36 +23,53 @@ Valid.AddTest = function AddTest(test) {
 
 Valid.ValidateQueue = function ValidateQueue(queue, value) {
     for(var i=0; i<queue.length; i++) {
-        var error = queue[i].call(this, value);
+       var error = queue[i].call(this, value);
         if(error) return error;
     }
 };
 
 
 // core api
+//     TODO: test?  check?  verify?  these names suck.
 
-Valid.validate = function validate(value) {
+// returns null if valid, the error string if invalid
+Valid.test = function test(value) {
     var self = this.GetChain();
-    var error = self.ValidateQueue(self._queue, value);
-    if(error && self._errorHandler) {
-        return self._errorHandler(value, error);
-    }
-    return !error;
+    return self.GetChain().ValidateQueue(self._queue, value);
 };
 
+// returns true if valid, false if invalid
+Valid.check = function check(value) {
+    return !this.test();
+}
 
-// custom error handlers
-
-Valid.throwErrors = function throwErrors() {
-    self = this.GetChain();
-    self._errorHandler = function ThrowErrors(value, message) {
-        throw value + " " + message;
-    };
-    return self;
-};
+// raises an error if invalid
+Valid.verify = function assert(value) {
+    var message = this.test(value);
+    if(message) throw value + " " + message;
+}
 
 
 // core tests
+
+// always fails validation
+Valid.fail = function fail(message) {
+    return this.GetChain().AddTest( function Fail(value) {
+        return message || "failed";
+    });
+};
+
+Valid.equal = function equal(wanted) {
+    return this.GetChain().AddTest( function Equal(value) {
+        if(value !== wanted) return "doesn't equal " + wanted;
+    });
+};
+
+Valid.typeOf = function typeOf(type) {
+    return this.GetChain().AddTest(function TypeOf(value) {
+        if(typeof value !== type) return "is of type " + (typeof value) + " not " + type;
+    });
+};
 
 Valid.and = function and() {
     var chains = arguments;
@@ -64,23 +81,15 @@ Valid.and = function and() {
     });
 };
 
-Valid.equal = function equal(wanted) {
-    return this.GetChain().AddTest( function Equal(value) {
-        if(value !== wanted) return "doesn't equal " + wanted;
-    });
-};
+// Valid.or
+// Valid.not
+
+Valid.todo = Valid.fail(" is still todo");
 
 //Valid.notEqual = Valid.not(Valid.equal);
-
-
-Valid.typeOf = function typeOf(type) {
-    return this.GetChain().AddTest(function TypeOf(value) {
-        if(typeof value !== type) return "is of type " + (typeof value) + " not " + type;
-    });
-};
-
+// Some JS implementations think typeof null === 'object'
 Valid.isUndefined   = Valid.equal(undefined);
-Valid.isNull        = Valid.equal(null);       // in some js impls typeof null === 'object'
+Valid.isNull        = Valid.equal(null);
 Valid.isBoolean     = Valid.typeOf('boolean');
 Valid.isNumber      = Valid.typeOf('number');
 Valid.isString      = Valid.typeOf('string');
