e9b20d7167cd216a0e8f1b06d8b2d9a589b934d1
Scott Bronson <brons_git@rinspin.com>
2011-09-20 16:29:16 -0700

    static arrays are mostly working

--- test/validjson.test.js
+++ test/validjson.test.js
@@ -63,23 +63,24 @@ Valid.json(/abc/  ).assert('In abc.');
 Valid.json(/abc/i ).assert('DEABCDEF');
 Valid.json(/abc/  ).assert('DEABCDEF', {".": {message:"does not match /abc/", value:"DEABCDEF"}});
 
-Valid.json( {abc: 123}           ).assert({abc: 123});
-Valid.json( {abc: 123, def: 456} ).assert({abc: 123},           {'.': {message: "is missing def", value: {"abc":123}}})
-Valid.json( {abc: 123}           ).assert({abc: 123, def: 456}, {'.': {message: "shouldn't have def", value: {"abc":123,"def":456}}})
-Valid.json( {abc: 123}           ).assert({}, {".":{message: "is missing abc", value: {}}});
-Valid.json( {}                   ).assert({abc: 123}, {".":{message: "shouldn't have abc", value: {"abc":123}}});
+Valid.json({abc: 123}          ).assert({abc: 123});
+Valid.json({abc: 123, def: 456}).assert({abc: 123},           {'.': {message: "is missing def", value: {"abc":123}}})
+Valid.json({abc: 123}          ).assert({abc: 123, def: 456}, {'.': {message: "shouldn't have def", value: {"abc":123,"def":456}}})
+Valid.json({abc: 123}          ).assert({}, {".":{message: "is missing abc", value: {}}});
+Valid.json({}                  ).assert({abc: 123}, {".":{message: "shouldn't have abc", value: {"abc":123}}});
 
 Valid.json({a: {b: {c: 1}}}).assert({a: {b: {c: 2}}}, {'a.b.c': {message: "does not equal 1", value: 2}})
 Valid.json({a: {b: /wut/i}}).assert({a: {b: "NOWUTY"}})
 
-/*
 // arrays
-schema( [12, 13] ).validate( [12, 13] ).result();
-schema( [12, 13] ).validate( [12, 14]  ).result("1: 14 does not equal 13 for 12,14"); // TODO improve error message
-schema( [12, 13] ).validate( 12        ).result("12 is not an Array");
-schema( [12, 13] ).validate( undefined ).result("undefined is not an Array");
-schema( [12, Validator.IsInteger] ).validate( [12, 13] ).result();
+Valid.json([12, 13]).assert([12, 13]);
+Valid.json([12, 13]).assert([12, 14],    {"[1]": {message: "does not equal 13", value: 14}});
+Valid.json([12, 13]).assert(12,          {".": {message:"is not an Array", value: 12}});
+Valid.json([12, 13]).assert(undefined,   {".": {message: "is not an Array", value: undefined}});
+Valid.json([12, 13]).assert(null,        {".": {message: "is null", value: null}});
+// Valid.json([12, Validator.integer()] ).validate( [12, 13] ).result();
 
+/*
 var _ = Validator;
 schema( _.IsArray(_.IsInteger) ).validate([12, 13, 14]).result();
 schema( _.IsArray(_.IsInteger, {min: 1, max: 3}) ).validate([12, 13]).result();
